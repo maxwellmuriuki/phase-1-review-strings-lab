7aca4bf4daf9f402ea70b2800fab3fda615b1521
Your Name <usamax089@gmail.com>
2024-09-23 14:20:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "maxwell";
+let welcomeMessage = "Welcome to Flatbook, maxwell !";
+let excitedWelcomeMessage= welcomeMessage.toLocaleUpperCase();
+const shortGreeting= "Welcome, m !";
+
